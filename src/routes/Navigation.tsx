580ce4d3d1dd32a9145eae58e673e13e9cd83c6b
Lalodev <laloing87@gmail.com>
2023-05-29 23:51:48 -0600

    kick off

---
+++ src/routes/Navigation.tsx
@@ -0,0 +1,56 @@
+import {
+  BrowserRouter,
+  Route,
+  NavLink,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+
+import logo from "../assets/react.svg";
+
+export const Navigation = () => {
+  return (
+    <BrowserRouter>
+      <div className="main-layout">
+        <nav>
+          <img src={logo} alt="React Logo" />
+          <ul>
+            <li>
+              <NavLink
+                to="/"
+                className={({ isActive }) => (isActive ? "activado" : "")}
+              >
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/about"
+                className={({ isActive }) => (isActive ? "activado" : "")}
+              >
+                About
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/users"
+                className={({ isActive }) => (isActive ? "activado" : "")}
+              >
+                Users
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Routes>
+          <Route path="/about" element={<h1>About</h1>}></Route>
+          <Route path="/users" element={<h1>Users</h1>}></Route>
+          <Route path="/" element={<h1>Home</h1>}></Route>
+          <Route path="/*" element={<Navigate to="/home" />}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+};
